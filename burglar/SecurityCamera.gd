cd79ca1a5949f91aadaaaee940f433bdc0c235a3
Nikolaj Baer <nikolaj.baer@gmail.com>
2018-08-11 18:12:08 +0200

    added security cams

--- burglar/SecurityCamera.gd
+++ burglar/SecurityCamera.gd
@@ -1,28 +1,12 @@
 extends Spatial
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-const X = 5
-const SPD = 0.2
-var flip = true
-var a = 0
-var detect
+export (NodePath) var target = null
+var _target = null
 
 func _ready():
-	detect = $Detect
+	if target != null:
+		_target = get_node(target)
 
 func _process(delta):
-	if flip:
-		if a > deg2rad(-45):
-			a -= deg2rad(SPD)
-		else:
-			flip = false
-	else:
-		if a < deg2rad(45):
-			a += deg2rad(SPD)
-		else:
-			flip = true
-	
-
-	detect.rotation.z = a
+	if _target != null:
+		look_at(_target.area.global_transform.origin,Vector3(0,0,1))
