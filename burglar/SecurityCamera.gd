86f442c3eb242cce3bcf237cd894a561f9266659
Nikolaj Baer <nikolaj.baer@gmail.com>
2018-08-11 18:25:50 +0200

    merged

--- burglar/SecurityCamera.gd
+++ burglar/SecurityCamera.gd
@@ -1,12 +1,28 @@
 extends Spatial
 
-export (NodePath) var target = null
-var _target = null
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+const X = 5
+const SPD = 0.2
+var flip = true
+var a = 0
+var detect
 
 func _ready():
-	if target != null:
-		_target = get_node(target)
+	detect = $Detect
 
 func _process(delta):
-	if _target != null:
-		look_at(_target.area.global_transform.origin,Vector3(0,0,1))
+	if flip:
+		if a > deg2rad(-45):
+			a -= deg2rad(SPD)
+		else:
+			flip = false
+	else:
+		if a < deg2rad(45):
+			a += deg2rad(SPD)
+		else:
+			flip = true
+	
+
+	detect.rotation.z = a
